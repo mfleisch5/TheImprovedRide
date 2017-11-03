c0eefb6fcdb0d1c0dc6afa1d283d8ba3aa85b1f3
oschlepphorst <schlepphorst.o@husky.neu.edu>
2017-11-03 11:06:25 -0400

    Added starter code including interfaces and test classes

---
+++ Source/TheRideRouteCalculator/src/Address.java
@@ -0,0 +1,37 @@
+/**
+ * Interface to represent an address in a trip.
+ */
+
+public interface Address {
+
+    /**
+     * Retrieve the address.
+     * @return The complete address including house number, street name, city, and zip code.
+     */
+    public String getAddress();
+
+    /**
+     * Retrieve the house number of the address.
+     * @return The house number as an integer.
+     */
+    public int getHousenumber();
+
+    /**
+     * Retrieve the street name of the address.
+     * @return The street name as a String.
+     */
+    public String getStreetName();
+
+    /**
+     * Retrieve the city of the address.
+     * @return The city as a String.
+     */
+    public String getCity();
+
+    /**
+     * Retrieve the zip code of the address.
+     * @return The zip code as an integer.
+     */
+    public int getZipCode();
+
+}
