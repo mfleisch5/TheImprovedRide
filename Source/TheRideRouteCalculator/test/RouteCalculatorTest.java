a08c4209b7aede8fda1e500ed48c247428da9899
Sophie Wigmore <wigmore.s@husky.neu.edu>
2017-11-08 09:54:28 -0500

    added route calc test for two trips in the same town at the same time, going to same destination, and two trips in same area, at similar times in order to test ridesharing

--- Source/TheRideRouteCalculator/test/RouteCalculatorTest.java
+++ Source/TheRideRouteCalculator/test/RouteCalculatorTest.java
@@ -68,12 +68,66 @@ public class RouteCalculatorTest {
 
     /**
      * Tests the route calculator for input where 4 trips are scheduled at the same time, 2 of which are in the same
-     * town and the other 2 are in a different town
+     * town and going to nearby destinations and the other 2 are in a different town, going to the same destination
      */
     @Test
     public void testTripsTwoSameTwoDiffTowns() {
+        Address pickup1 = new AddressImpl(149, "Sutherland Road", "Brighton", 2135);
+        Address dropoff1 = new AddressImpl(63, "Everett Street", "Allston", 2134);
+
+        Address pickup2 = new AddressImpl(55, "Cummings Road", "Brighton", 2135);
+        Address dropoff2 = new AddressImpl(98, "Saunders Street", "Allston", 2134);
+
+        Address pickup3 = new AddressImpl(21, "High Street", "Hingham", 2043);
+        Address dropoff3 = new AddressImpl(2, "Pond Park Road", "Hingham", 2043);
 
 
+        Address pickup4 = new AddressImpl(150, "Prospect Street", "Hingham", 2043);
+        Address dropoff4 = new AddressImpl(2, "Pond Park Road", "Hingham", 2043);
+
+        Trip trip1 = new TripImpl(1, false, false, true,
+                "3:00 PM", 1, 0, false, pickup1, dropoff1);
+
+        Trip trip2 = new TripImpl(14, false, true, false, "3:05 PM",
+                1, 0, false, pickup2, dropoff2);
+
+        Trip trip3 = new TripImpl(22, false, false, true, "11:15 AM",
+                0, 0, false, pickup3, dropoff3);
+
+        Trip trip4 = new TripImpl(14, false, true, false, "11:15 AM",
+                0, 0, false, pickup4, dropoff4);
+
+
+        List<Trip> trips = new ArrayList<Trip>();
+        trips.add(trip1);
+        trips.add(trip2);
+        trips.add(trip3);
+        trips.add(trip4);
+
+        RouteCalculator calculator = new RouteCalculatorImpl(trips);
+
+        // Calculate a schedule
+        calculator.routeCalculator();
+
+
+        /**
+         * Need to figure out how to represent rideshare ride - trip
+         */
+        String answer = "Route 1:\n" +
+                "Pickup 3:00 PM, Trip ID 1, 149 Sutherland Road, Brighton, 2135\n" +
+                "Dropoff, Trip ID 1, 63 Everett Street, Allston, 2134\n" +
+                "Route 2:\n" +
+                "Pickup 3:05P M, Trip ID 14, 55 Cummings Road, Brighton, 2135\n" +
+                "Dropoff, Trip ID 14, 98 Saunders Street, Allston, 2134\n" +
+                "Route 3:\n" +
+                "Pickup 11:15 AM, Trip ID 22, 21 High Street, Hingham, 2043\n" +
+                "Dropoff, Trip ID 22, 2 Pond Park Road, Hingham, 2043\n" +
+                "Route 4:\n" +
+                "Pickup 2:00 PM, Trip ID 14, 150 Prospect Street , Hingham, 2043\n" +
+                "Dropoff, Trip ID 14, 2 Pond Park Road, Hingham, 2043";
+
+        assertEquals("Expected output schedule should match calculated one",
+                answer, calculator.outPutRoutes());
     }
 
 }
