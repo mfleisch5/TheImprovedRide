460be9142bd848fb5b68139a0b39ee8f4a9a4c9e
Michael Fleischmann <mfleisch50@gmail.com>
2017-11-30 17:40:14 -0500

    New backend functionality

--- PythonTheRide/backend.py
+++ PythonTheRide/backend.py
@@ -1,13 +1,13 @@
 from flask import Flask, request
-import RoutingCalculator
+import RoutingCalculator, json
 
 app = Flask(__name__)
 
 
 @app.route('/post', methods=['POST'])
-def get_json():
-    in_dict = request.json['trips']
-    return str(RoutingCalculator.main(in_dict, 'geocodes.json', 'failures.json', 3))
+def get_data():
+    in_records = request.json['trips']
+    return json.dumps(RoutingCalculator.main(in_records, 'geocodes.json', 'failures.json', 3).to_json_format())
 
 app.debug = True
 app.run(host='0.0.0.0', port=5000)
