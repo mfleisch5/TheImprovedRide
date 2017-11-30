634fb70e8cb172096ee64dcf4d5e0f0acea96b35
Michael Fleischmann <mfleisch50@gmail.com>
2017-11-30 18:23:19 -0500

    Fixed backend bugs

--- PythonTheRide/backend.py
+++ PythonTheRide/backend.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
-import RoutingCalculator, json
+import RoutingCalculator, json, os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 app = Flask(__name__)
 
@@ -7,10 +9,8 @@ app = Flask(__name__)
 @app.route('/post', methods=['POST'])
 def get_data():
     in_records = request.json['trips']
-    j_format = json.dumps(RoutingCalculator.main(in_records, 'geocodes.json', 'failures.json', 3).to_json_format())
-    print("--------")
-    print(j_format)
-    return json.dumps(RoutingCalculator.main(in_records, 'geocodes.json', 'failures.json', 3).to_json_format())
+    return json.dumps(RoutingCalculator.main(in_records, os.path.join(dir_path, 'geocodes.json'),
+                                             os.path.join(dir_path,'failures.json'), 3).to_json_format())
 
 app.debug = True
 app.run(host='0.0.0.0', port=5000)
